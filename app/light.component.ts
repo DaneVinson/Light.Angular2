ccb801488b92f3117ff2300a6d77950c379be4c4
DaneVinson <dane.vinson@gmail.com>
2016-02-06 23:29:35 -0800

    Small touches

--- app/light.component.ts
+++ app/light.component.ts
@@ -5,6 +5,6 @@ import {Component} from 'angular2/core';
   templateUrl: 'app/light.html'
 })
 export class LightComponent {
-  public title = 'Light v4, Angular 2';
-  public description = "Angular 2 implementation of the Light exploratory application."
+  public title = 'Light 4, Angular 2';
+  public description = "Angular 2 implementation of the Light concept application (v4)."
 }
